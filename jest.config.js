bfd7cb59a1a580e207e61a293f6640055d5e1273
Joe Alvarado <joea3121@gmail.com>
2026-06-03 14:05:18 -0500

    Setting up CI/CD Pipeline

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+};
